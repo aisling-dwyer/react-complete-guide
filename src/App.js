ec355844bbeaede7d1af8d79eafeca195d3d5c28
Aisling Dwyer <x21123748@student.ncirl.ie>
2022-09-13 12:10:05 +0100

    useState utilised + new components created NewExpense & ExpenseForm

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
-import Expenses from "./components/Expenses";
+import React from 'react';
+
+import NewExpense from './components/NewExpense/NewExpense';
+import Expenses from "./components/Expenses/Expenses";
 
 function App() {
 
@@ -32,7 +35,7 @@ function App() {
 
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense />
       <Expenses items={expenses}/>
     </div>
   );
